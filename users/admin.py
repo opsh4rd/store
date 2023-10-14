67ad8bbc15c8692ddb8eaad595fe19fb60b97570
Ruslan <miftakhov.ruslan.m@gmail.com>
2023-10-14 18:42:20 +0300

    add_admin_panel

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from users.models import User
 
-admin.site.register(User)
+from products.admin import BasketAdmin
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username',)
+    inlines = (BasketAdmin,)
